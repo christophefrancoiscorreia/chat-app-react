9efbe56e5ee61100ac6e3e4846ddc00bcfe3f293
Christophe Correia <cc8design@gmail.com>
2020-10-15 14:43:36 +0200

    after chat

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MessageList from './components/MessageList'
+//import SendMessageForm from './components/SendMessageForm'
+//import RoomList from './components/RoomList'
+//import NewRoomForm from './components/NewRoomForm'
+//import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MessageList />
     </div>
   );
 }
